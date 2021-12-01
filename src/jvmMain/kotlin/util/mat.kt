8198abde7d00423cbc7c90c74949dfeeccbbe33d
Martynas Petuška <martynas@petuska.dev>
2021-12-01 00:44:23 +0000

    Working Stream

--- src/jvmMain/kotlin/util/mat.kt
+++ src/jvmMain/kotlin/util/mat.kt
@@ -11,21 +11,35 @@ import kotlin.math.floor
 import org.opencv.core.Mat
 
 fun Mat.to2dBytes(): ByteArray {
-  val buffer =
-      Array(height()) { y ->
-        ByteArray(width() * 2).also { ba ->
-          for (x in 0 until ceil(ba.size / 2.0).toInt()) {
-            ba[x * 2] = this[x, y, 0].toInt().toByte()
-          }
-          for (x in 0 until ceil(ba.size / 4.0).toInt()) {
-            ba[x * 4 + 1] = this[x * 2, y, 1].toInt().toByte()
-          }
-          for (x in 0 until floor(ba.size / 4.0).toInt()) {
-            ba[x * 4 + 3] = this[x * 2, y, 2].toInt().toByte()
-          }
-        }
-      }
-  return buffer.flatMap { it.toList() }.toByteArray()
+  //  val buffer =
+  //      Array(height()) { y ->
+  //        ByteArray(width() * 2).also { ba ->
+  //          for (x in 0 until ceil(ba.size / 2.0).toInt()) {
+  //            ba[x * 2] = this[x, y, 0].toInt().toByte()
+  //          }
+  //          for (x in 0 until ceil(ba.size / 4.0).toInt()) {
+  //            ba[x * 4 + 1] = this[x * 2, y, 1].toInt().toByte()
+  //          }
+  //          for (x in 0 until floor(ba.size / 4.0).toInt()) {
+  //            ba[x * 4 + 3] = this[x * 2, y, 2].toInt().toByte()
+  //          }
+  //        }
+  //      }
+  //  return buffer.flatMap { it.toList() }.toByteArray()
+
+  val barr = ByteArray(height() * width() * 2)
+  for (y in 0 until height()) {
+    for (x in 0 until ceil(width() * 2 / 2.0).toInt()) {
+      barr[(y * width() * 2) + x * 2] = this[x, y, 0].toInt().toByte()
+    }
+    for (x in 0 until ceil(width() * 2 / 4.0).toInt()) {
+      barr[(y * width() * 2) + x * 4 + 1] = this[x * 2, y, 1].toInt().toByte()
+    }
+    for (x in 0 until floor(width() * 2 / 4.0).toInt()) {
+      barr[(y * width() * 2) + x * 4 + 3] = this[x * 2, y, 2].toInt().toByte()
+    }
+  }
+  return barr
 }
 
 fun Mat.toBufferedImage(): BufferedImage {
